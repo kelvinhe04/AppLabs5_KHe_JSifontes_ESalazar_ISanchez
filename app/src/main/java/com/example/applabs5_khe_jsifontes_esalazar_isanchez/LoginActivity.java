06922832aa6a74a8fb7ab38e585e75bade4f2f31
Kelvin He Wu <91310516+kelvinhe04@users.noreply.github.com>
2025-06-18 23:49:42 -0500

    Refactor: Rename MainActivity to BienvenidaActivity and add LoginActivity and CrearUsuarioActivity
    MainActivity has been renamed to BienvenidaActivity and its layout has been updated to activity_bienvenida.
    New activities LoginActivity and CrearUsuarioActivity have been added.

---
+++ app/src/main/java/com/example/applabs5_khe_jsifontes_esalazar_isanchez/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.applabs5_khe_jsifontes_esalazar_isanchez;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
